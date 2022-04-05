e81fb6d59310ecb0e820ef6c96c567bd5c8b9072
tiancai <sky0975657713@gmail.com>
2022-04-05 21:03:37 +0800

    home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../componet/Layout";
-
+import "antd/dist/antd.css";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,42 @@
+import { Button } from "antd";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import logo from "../image/logo.jpg";
+import loading from "../image/loading.png";
 const Home: NextPage = () => {
   return (
-    <>
-      <div className={styles.container}></div>
-    </>
+    <div className={styles.container}>
+      <div className={styles.logo}>
+        <div className={styles.box1}>
+          <Image src={logo.src} layout="fill" />
+        </div>
+        <div className={styles.box2}>
+          <Image src={loading.src} layout="fill" />
+        </div>
+      </div>
+      <div className={styles.title}>
+        <h1>welcome</h1>
+        <p>My Front End Blog : )</p>
+        <div className={styles.loginBtn}>
+          <Button type="primary" shape="round" size={"middle"} ghost>
+            visitor
+          </Button>
+          <Button
+            type="primary"
+            shape="round"
+            size={"middle"}
+            ghost
+            onClick={() => {
+              console.log(logo);
+            }}
+          >
+            Log in
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
 
