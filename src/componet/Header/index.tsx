1b024312a421bdb38061318ab3619080cc6a1486
tiancai_1228 <sky0975657713@gmail.com>
2022-05-11 14:30:54 +0800

    log out

--- src/componet/Header/index.tsx
+++ src/componet/Header/index.tsx
@@ -2,17 +2,26 @@ import React, { useState, useEffect } from "react";
 import styles from "../../styles/Header.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import Cookies from "js-cookie";
 function Header() {
   let location = useRouter();
 
   const [headerType, setHeaderType] = useState("header");
+  const [isLogin, setIsLogin] = useState<any>();
+  // const isLogin = Cookies.get("login");
+
+  const handelLogOut = () => {
+    Cookies.remove("login");
+    setIsLogin(Cookies.get("login"));
+  };
+
   useEffect(() => {
-    console.log(location.pathname);
     if (location.pathname === "/") {
       setHeaderType("header");
     } else {
       setHeaderType("header_home");
     }
+    setIsLogin(Cookies.get("login"));
   });
 
   return (
@@ -37,14 +46,28 @@ function Header() {
         </li>
         <li>
           <Link href="/blog">
-            <a className={styles.h3}>Blog</a>
+            <a className={styles.h3}>chat</a>
           </Link>
         </li>
-        <li>
-          <Link href="/signUp">
-            <a className={styles.h3}>Sign Up</a>
-          </Link>
-        </li>
+        {!isLogin && (
+          <li>
+            <Link href="/signUp">
+              <a className={styles.h3}>SignUp/LogIn</a>
+            </Link>
+          </li>
+        )}
+        {isLogin && (
+          <li>
+            <a
+              className={styles.h3}
+              onClick={() => {
+                handelLogOut();
+              }}
+            >
+              Log out
+            </a>
+          </li>
+        )}
       </ul>
     </div>
   );
